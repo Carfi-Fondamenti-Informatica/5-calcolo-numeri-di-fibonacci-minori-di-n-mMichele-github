816b06d021924f200db5a1b4b45086c59051544c
mMichele-github <100371043+mMichele-github@users.noreply.github.com>
2022-04-05 14:11:03 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+int fibonacci(int a)
+{
+    int p=0, q=1, ris=0;
+    if (a>=2)
+    {
+        cout << p << endl << q << endl;
+        for(int i=3; i<=a; i++)
+        {
+            ris = p + q;
+            cout << ris << endl;
+            p = q;
+            q = ris;
+        }
+    }
+    else
+    {
+        cout <<"errore"<< endl;
+    }
+    return 0;
+}
+
+int main()
+{
+    int n=0;
+    cout << "Stabilire dopo quanti passaggi fermarsi nella sequenza di Fibonacci: " << endl;
+    cin >> n;
+    int sequenza = fibonacci(n);
+    return sequenza;
 }
